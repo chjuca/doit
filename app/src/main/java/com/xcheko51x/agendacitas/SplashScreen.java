ceedb911cdc6a45453bb8ff58bd19672172d33ee
renatojobal <renatojobal@gmail.com>
2019-11-09 18:30:37 -0500

    Ya funciona el login, aun falta ponerle estilos

--- app/src/main/java/com/xcheko51x/agendacitas/SplashScreen.java
+++ app/src/main/java/com/xcheko51x/agendacitas/SplashScreen.java
@@ -33,7 +33,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, Navegacion.class);
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
