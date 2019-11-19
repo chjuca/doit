c966058b2b0e421541218fbb922a59759f3a23b6
David <jhandry13@hotmail.com>
2019-11-19 10:17:43 -0500

    ahora si esta el calendario 👍

---
+++ app/src/main/java/com/xcheko51x/agendacitas/pruebaCalendario.java
@@ -0,0 +1,18 @@
+package com.xcheko51x.agendacitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import sun.bob.mcalendarview.MCalendarView;
+import sun.bob.mcalendarview.MarkStyle;
+import sun.bob.mcalendarview.vo.DateData;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class pruebaCalendario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba_calendario);
+    }
+}
