51d01dc4c35f720de45025ab255d24a941a6eb12
David <jhandry13@hotmail.com>
2019-11-25 10:05:26 -0500

    subo nada para bajar cambios

--- app/src/main/java/com/xcheko51x/agendacitas/pruebaCalendario.java
+++ app/src/main/java/com/xcheko51x/agendacitas/pruebaCalendario.java
@@ -1,7 +1,9 @@
 package com.xcheko51x.agendacitas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import sun.bob.mcalendarview.MCalendarView;
 import sun.bob.mcalendarview.MarkStyle;
 import sun.bob.mcalendarview.vo.DateData;
@@ -14,9 +16,18 @@ import android.widget.Toast;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.xcheko51x.agendacitas.Adaptadores.AdaptadorCitas;
+import com.xcheko51x.agendacitas.Models.Events;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -24,6 +35,12 @@ import java.util.Locale;
 public class pruebaCalendario extends AppCompatActivity {
 
     CompactCalendarView compactCalendar;
+    DatabaseReference databaseReference;
+    final List<Events> listaEvents = new ArrayList<>();
+    AdaptadorCitas adaptador;
+    RecyclerView rvCitas;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
     private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
 
     @Override
@@ -37,6 +54,10 @@ public class pruebaCalendario extends AppCompatActivity {
 
         compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
         compactCalendar.setUseThreeLetterAbbreviation(true);
+
+
+
+
 
         Event ev1 = new Event(Color.GREEN, 1575057600000L, "Evento de Prueba");
         compactCalendar.addEvent(ev1);
@@ -66,5 +87,31 @@ public class pruebaCalendario extends AppCompatActivity {
 
             }
         });
+
+            }
+
+    public void obtenerEventos(){
+
+        databaseReference.child("Events").orderByChild("evCreateUser").equalTo(user.getEmail()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listaEvents.clear();
+                for(DataSnapshot objSnapshot: dataSnapshot.getChildren()) {
+
+                    Events events = objSnapshot.getValue(Events.class);                              // GET DE EVENTOS
+                    listaEvents.add(events);
+
+                    //adaptador = new AdaptadorCitas(getContext(), listaEvents);
+                    rvCitas.setAdapter(adaptador);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
