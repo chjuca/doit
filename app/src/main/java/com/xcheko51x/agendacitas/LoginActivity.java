1873546c662f0079127eca60f3fd8ed4d08e51f3
renatojobal <renatojobal@gmail.com>
2019-11-26 01:16:44 -0500

    Nueva funcionalidad: cerrar sesion

--- app/src/main/java/com/xcheko51x/agendacitas/LoginActivity.java
+++ app/src/main/java/com/xcheko51x/agendacitas/LoginActivity.java
@@ -134,7 +134,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-
+                            StaticData.currentUser = user; // Guardamos el usuario logeado en el archivo static
                             updateUI(user);
 
                         } else {
@@ -161,5 +161,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Intent intent = new Intent(LoginActivity.this, NotificationActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
