3417cf23ecfa9704eec3063680c6015fc014370c
renatojobal <renatojobal@gmail.com>
2019-12-08 10:00:02 -0500

    Agregando metodos de control

--- app/src/main/java/com/doitutpl/doit/Controllers/GroupsController.java
+++ app/src/main/java/com/doitutpl/doit/Controllers/GroupsController.java
@@ -1,4 +1,42 @@
 package com.doitutpl.doit.Controllers;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.doitutpl.doit.Models.Group;
+import com.doitutpl.doit.StaticData;
+import com.google.firebase.database.DatabaseReference;
+
 public class GroupsController {
+
+    // Metodo para validar los datos antes de subirlo a la base de datos
+    public boolean validateData(Group group){
+        /*Aqui se debe validar que los datos esten correctos*/
+        boolean isValidData = true;
+        if(group.keyGroup==null || group.chat==null || group.password==null || group.groupAdminEmail==null){
+            isValidData=false;
+            Log.println(Log.ERROR,"Invalid Data", "No valid data for save Object Group into firebase");
+        }
+        return isValidData;
+    }
+
+
+    public void save(Group group, Context context){
+        /* Metodo para guardar el grupo en la base de datos */
+
+        // Validamos la informacion
+        if(validateData(group)){
+
+            // Obtenemos la conexion
+            DatabaseReference databaseReference = Connection.initializeFirebase(context);
+
+            // Lo guardamos en la base de datos
+            databaseReference.child(StaticData.groupsNodeTitle).child(group.getKeyGroup()).setValue(group);
+
+        }
+
+    }
+
+
+
 }
--- app/src/main/java/com/doitutpl/doit/Models/Group.java
+++ app/src/main/java/com/doitutpl/doit/Models/Group.java
@@ -3,8 +3,10 @@ package com.doitutpl.doit.Models;
 import android.content.Context;
 import android.util.Log;
 
+import com.doitutpl.doit.Controllers.GroupsController;
 import com.doitutpl.doit.StaticData;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -16,29 +18,12 @@ public class Group {
     public Member[] members;
     public String chat;
     public String password;
-    public User groupAdmin;
-
-    // Atributos para la conexión
-    private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference databaseReference;
-
-    // Atributos de control
-    boolean isValidData = false;
+    public String groupAdminEmail;
 
 
 
     public Group(){
         /*Constructor*/
-    }
-
-
-
-    // Constructor para usarse desde la UI
-    public Group(String keyGroup, String chat, String password, User groupAdmin) {
-        this.keyGroup = keyGroup;
-        this.chat = chat;
-        this.password = password;
-        this.groupAdmin = groupAdmin;
     }
 
     public String getKeyGroup() {
@@ -70,57 +55,31 @@ public class Group {
     }
 
     public void setPassword(String password) {
+        this.password = password;    // Atributos de control
+    boolean isValidData = false;
+
+    }
+
+    public String getGroupAdminEmail() {
+        return groupAdminEmail;
+    }
+
+    public void setGroupAdminEmail(String groupAdminEmail) {
+        this.groupAdminEmail = groupAdminEmail;
+    }
+
+    // Constructor para usarse desde la UI
+    public Group(String keyGroup, String chat, String password, FirebaseUser groupAdmin) {
+        this.keyGroup = keyGroup;
+        this.chat = chat;
         this.password = password;
+        this.groupAdminEmail = groupAdmin.getEmail();
     }
 
-    public User getGroupAdmin() {
-        return groupAdmin;
-    }
-
-    public void setGroupAdmin(User groupAdmin) {
-        this.groupAdmin = groupAdmin;
-    }
-
-
-
-    public void save(Context context, User groupAdmin){
-        /* Metodo para guardar el grupo en la base de datos */
-
-        // Agregamos el usuario como admin del grupo
-        this.groupAdmin = groupAdmin;
-
-        // Verificamos que la data del grupo sea correcta
-        if(this.validateData()){
-            // Inicilizamos la conexión con la base de datos
-            initializeFirebase(context);
-
-            // Lo guardamos en la base de datos
-            databaseReference.child(StaticData.groupsNodeTitle).child(this.getKeyGroup()).setValue(this);
-        }
-
-    }
-
-    //
-    private void initializeFirebase(Context context) {
-
-        FirebaseApp.initializeApp(context);
-
-
-
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference();
-    }
-
-
-    // Metodo para validar los datos antes de subirlo a la base de datos
-    public boolean validateData(){
-        /*Aqui se debe validar que los datos esten correctos*/
-        this.isValidData = true;
-        if(keyGroup==null || chat==null || password==null || groupAdmin==null){
-            isValidData=false;
-            Log.println(Log.ERROR,"Invalid Data", "No valid data for save Object Group into firebase");
-        }
-        return isValidData;
+    // Metodo para guardarse en la base de datos
+    public void save(Context context){
+        GroupsController groupsController = new GroupsController();
+        groupsController.save(this, context);
     }
 
     /* Metodo para construir este objeto a partir ded un JSON de la base de datos
