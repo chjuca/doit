22ce8871152f3401caa3696ee21a9af526a942f0
renatojobal <renatojobal@gmail.com>
2020-01-21 00:07:58 -0500

    Metodo isFirstLogin creado

--- app/src/main/java/com/doitutpl/doit/ui/tutorial/TutorialActivity.java
+++ app/src/main/java/com/doitutpl/doit/ui/tutorial/TutorialActivity.java
@@ -1,9 +1,12 @@
 package com.doitutpl.doit.ui.tutorial;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.doitutpl.doit.R;
+import com.doitutpl.doit.StaticData;
 import com.doitutpl.doit.ui.SplashScreen;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -12,6 +15,7 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Trace;
 import android.view.View;
 
 public class TutorialActivity extends AppCompatActivity {
@@ -43,12 +47,27 @@ public class TutorialActivity extends AppCompatActivity {
             // Saltar a la pantalla de login
             goToNextActivity();
         }
-
+        // Continuar con el tutorial
     }
 
     private boolean isFirstLogin() {
         // Revisar la variable en los shared preferences
 
+        // Ingresamos a las preferencias compartidas
+        SharedPreferences prefs =
+                getSharedPreferences(StaticData.USER_PREFERENCES_NAME, Context.MODE_PRIVATE);
+
+
+        // Obtenemos la variable que verifica si el usuario ya ha ingresado anteriormente
+        String previousLogin = prefs.getString(StaticData.IS_FIRST_LOGIN, "False");
+
+
+        if(previousLogin.equals("False")){
+            return false;
+        }
+
+
+        return true;
     }
 
 
