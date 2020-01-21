61ac6a2648e376f49a5756d144cfcf35fb2e6a08
renatojobal <renatojobal@gmail.com>
2020-01-20 23:57:52 -0500

    Creando la estrucutra de los métodos

--- app/src/main/java/com/doitutpl/doit/ui/tutorial/TutorialActivity.java
+++ app/src/main/java/com/doitutpl/doit/ui/tutorial/TutorialActivity.java
@@ -1,8 +1,10 @@
 package com.doitutpl.doit.ui.tutorial;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.doitutpl.doit.R;
+import com.doitutpl.doit.ui.SplashScreen;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -18,6 +20,10 @@ public class TutorialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+
+
+
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
@@ -26,4 +32,32 @@ public class TutorialActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
+    // Método que controla sis presentar el tutorial o saltar directamente al login
+    private void manageTutorial(){
+
+        if(!isFirstLogin()){ // Si es la primera vez que se logea, debería mostrarle el tutorial, si no saltar a la pantalla de login
+            // Saltar a la pantalla de login
+            goToNextActivity();
+        }
+
+    }
+
+    private boolean isFirstLogin() {
+        // Revisar la variable en los shared preferences
+
+    }
+
+
+
+    private void goToNextActivity(){
+        Intent intent = new Intent(TutorialActivity.this, SplashScreen.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
